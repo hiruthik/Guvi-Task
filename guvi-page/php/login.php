f5d6da3537d56f93024dbfd8cb81e49835e19aa9
hiruthik <82458215+hiruthik@users.noreply.github.com>
2023-11-27 15:59:09 +0530

    Update login.php

--- guvi-page/php/login.php
+++ guvi-page/php/login.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-// Establish a MySQL database connection
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,36 +10,36 @@ $dbname = "project";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve login form data
+
 $email = $_POST['email'];
 $enteredPassword = $_POST['password'];
 
-// Retrieve hashed password from the database based on the provided email
+
 $sql = "SELECT id, name, email, password FROM users WHERE email = '$email'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // User found, check password
+    
     $row = $result->fetch_assoc();
     $hashedPassword = $row['password'];
 
     if (password_verify($enteredPassword, $hashedPassword)) {
-        // Password is correct, login successful
+        
         echo "Login successful";
     } else {
-        // Password is incorrect
+        
         echo "Incorrect credentials";
     }
 } else {
-    // User not found
+    
     echo "Incorrect credentials";
 }
 
-// Close the database connection
+
 $conn->close();
 ?>
