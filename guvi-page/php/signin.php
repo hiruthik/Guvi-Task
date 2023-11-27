71745bdbacbdba3a8ae40751cc3773b4bdfce207
hiruthik <82458215+hiruthik@users.noreply.github.com>
2023-11-27 16:01:29 +0530

    Update signin.php

--- guvi-page/php/signin.php
+++ guvi-page/php/signin.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-// Establish a MySQL database connection
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,18 +10,18 @@ $dbname = "project";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 
-// Retrieve form data
+
 $fullName = $_POST['name'];
 $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-// Insert data into the database
+
 $sql = "INSERT INTO users ( id, name, email, password) VALUES ( 1,'$fullName', '$email', '$password')";
 
 if ($conn->query($sql) === TRUE) {
@@ -30,6 +30,6 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close the database connection
+
 $conn->close();
 ?>
