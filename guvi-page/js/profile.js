6eea347ef7dea2d7fda4f6551135bc7b37cb418d
hiruthik <82458215+hiruthik@users.noreply.github.com>
2023-11-28 12:23:44 +0530

    Delete guvi-page directory

--- guvi-page/js/profile.js
+++
@@ -1,8 +0,0 @@
-const animationPath = 'female-avatar.json';
-    const animation = lottie.loadAnimation({
-      container: document.getElementById('lottieContainer'),
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: animationPath,
-    });
